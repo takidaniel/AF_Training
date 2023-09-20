33a5cd2f86a63f68e8ca08b999adcd1d37e8332b
Daniel Mieszkowski (Sii) <dmieszkowski@sii.pl>
2023-09-20 16:24:07 +0200

    trigger2

--- TShttpTrigger1/src/functions/TShttpTrigger1.ts
+++ TShttpTrigger1/src/functions/TShttpTrigger1.ts
@@ -8,8 +8,22 @@ export async function TShttpTrigger1(request: HttpRequest, context: InvocationCo
     return { body: `Hello, ${name}!` };
 };
 
+export async function TShttpTrigger2(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello 2, ${name}!` };
+};
+
 app.http('TShttpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: TShttpTrigger1
+});
+
+app.http('TShttpTrigger2', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: TShttpTrigger2
 });
